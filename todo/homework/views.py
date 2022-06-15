bc78e8d104be889c47878f37140d1d124ed0ba74
TokonovaGulmira <tilekarzi0621@gmail.com>
2022-06-15 17:51:50 +0600

    static files

--- todo/homework/views.py
+++ todo/homework/views.py
@@ -6,3 +6,5 @@ def newf(request):
     return HttpResponse("WElCOME TO THE APP TODO-ADMIN")
 def homepage(request):
     return render(request,"index.html")
+def homework(request):
+    return render(request,"homework.html")
