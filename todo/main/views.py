a056ed3b4e8c4499329f28313f2ef6aa2dad4f7e
TokonovaGulmira <tilekarzi0621@gmail.com>
2022-06-15 13:58:22 +0600

    copied index.html

--- todo/main/views.py
+++ todo/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request,"index.html")
+    return render(request,"ind.html")
 
 def test(request):
     return render(request,"test.html")
